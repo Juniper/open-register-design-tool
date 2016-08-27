ec979dec977797fbe020ed6f6fc141886b45b3f1
Scott Nellenbach <snellenbach@yahoo.com>
2016-08-27 14:10:46 -0400

    rev bump

--- src/ordt/extract/Ordt.java
+++ src/ordt/extract/Ordt.java
@@ -25,7 +25,7 @@ import ordt.parameters.ExtParameters;
 
 public class Ordt {
 
-	private static String version = "160826.01"; 
+	private static String version = "160827.01"; 
 	private static DebugController debug = new MyDebugController(); // override design annotations, input/output files
 
 	public enum InputType { RDL, JSPEC };
