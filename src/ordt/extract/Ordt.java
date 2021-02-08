ded768db3ce928dade50df383186867aaae76cf2
Scott Nellenbach <snellenbach@gmail.com>
2021-02-07 22:18:22 -0500

    Update allows hardware changes to take effect when occuring in same cycle as rclr/rset (this was already the case for counter fields).  If previous behavior is needed, 'precedence=sw' should be added to the rclr/rset field.

--- src/ordt/extract/Ordt.java
+++ src/ordt/extract/Ordt.java
@@ -36,7 +36,7 @@ import ordt.parameters.ExtParameters.UVMModelModes;
 
 public class Ordt {
 
-	private static String version = "201029.01";
+	private static String version = "210207.01";
 	private static DebugController debug = new MyDebugController(); // override design annotations, input/output files
 
 	public enum InputType { RDL, JSPEC };
